a70b571cc485d6a9b5d8fd5844777e87fb6d8b64
adelinel <adeline.latruwe@gmail.com>
2021-03-23 13:20:52 -0700

    added NavBar component + tests

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import { Component } from "react";
 import { BrowserRouter } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
 import LandingPage from "./components/LandingPage/LandingPage";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      auth: false,
+      hasError: false,
+    };
+  }
+
+  handleAuthSubmit = () => {
+    this.setState({ auth: true });
+  };
+
+  handleLogOut = () => {
+    this.setState({ auth: false });
+  };
+
   render() {
+    const auth = this.state.auth;
     return (
       <BrowserRouter>
+        <NavBar auth={auth} handleLogOut={this.handleLogOut}></NavBar>
         <LandingPage></LandingPage>
       </BrowserRouter>
     );
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import "./NavBar.css";
+
+class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: false,
+    };
+  }
+
+  // navbar toggle on and off
+  toggleClass = () => {
+    this.state.active
+      ? this.setState({ active: false })
+      : this.setState({ active: true });
+  };
+
+  renderLogoutNav() {
+    return (
+      <div className={this.state.active ? "active" : "main-nav"}>
+        <Link to="/dashboard" className="nav-links">
+          Dashboard
+        </Link>
+        <br />
+        <Link to="/new-entry" className="nav-links">
+          New entry
+        </Link>
+        <br />
+        <Link onClick={this.props.handleLogOut} to="/" className="nav-links">
+          Logout
+        </Link>
+        <br />
+      </div>
+    );
+  }
+
+  renderLoginNav() {
+    return (
+      <div className={this.state.active ? "active" : "main-nav"}>
+        <Link to="/signup" className="nav-links">
+          Sign Up
+        </Link>
+        <br />
+        <Link
+          to={{
+            pathname: `/login`,
+            demo: true,
+          }}
+          className="nav-links"
+        >
+          Demo
+        </Link>
+        <br />
+        <Link to="/login" className="nav-links">
+          Log in
+        </Link>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <>
+        <nav className="navbar">
+          <div className="navbar-toggle" onClick={this.toggleClass}>
+            <i className="fas fa-bars"></i>
+          </div>
+          <div className="logo">
+            <i className="fas fa-layer-group"></i>My Learning Stack
+          </div>
+          {this.props.auth ? this.renderLogoutNav() : this.renderLoginNav()}
+        </nav>{" "}
+      </>
+    );
+  }
+}
+
+export default NavBar;
