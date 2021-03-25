e3bc974c6c4bbccd04704dc355d79e4886a2c086
adelinel <adeline.latruwe@gmail.com>
2021-03-25 10:28:13 -0700

    changed Profile component with form inputs

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -6,7 +6,9 @@ class Profile extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      tech: 0,
+      tech1: "",
+      tech2: "",
+      tech3: "",
     };
   }
   static defaultProps = {
@@ -16,14 +18,23 @@ class Profile extends Component {
   };
 
   validateTech = () => {
-    if (this.state.tech === 0) {
+    if (
+      this.state.tech1 === "" &&
+      this.state.tech2 === "" &&
+      this.state.tech3 === ""
+    ) {
       return "Please chose at least 1 tech you work with";
     }
   };
 
-  updateTech = () => {
-    const count = this.state.tech + 1;
-    this.setState({ tech: count });
+  updateTech1 = (tech) => {
+    this.setState({ tech1: tech });
+  };
+  updateTech2 = (tech) => {
+    this.setState({ tech2: tech });
+  };
+  updateTech3 = (tech) => {
+    this.setState({ tech3: tech });
   };
 
   handleSubmit = (ev) => {
@@ -38,31 +49,45 @@ class Profile extends Component {
         <h1 className="form-title">
           <span>My Profile</span>
         </h1>
+
         <div className="form">
+          <p>I work with:</p>
           <form className="form-details" onSubmit={this.handleSubmit}>
-            <button onClick={this.updateTech} type="button" className="btn">
-              <i className="fab fa-html5">HTML</i>
-            </button>
-            <button onClick={this.updateTech} type="button" className="btn">
-              <i className="fab fa-css3-alt">CSS</i>
-            </button>
-            <button onClick={this.updateTech} type="button" className="btn">
-              <i className="fab fa-js">Javascript</i>
-            </button>
-            <button onClick={this.updateTech} type="button" className="btn">
-              <i className="fab fa-react">React</i>
-            </button>
-            <button onClick={this.updateTech} type="button" className="btn">
-              <i className="fab fa-node-js">Node.js</i>
-            </button>
-            {this.state.tech === 0 && <ValidationError message={techError} />}
+            <input
+              type="text"
+              id="tech1"
+              name="tech1"
+              placeholder="React"
+              className="rounded-input"
+              value={this.state.tech1}
+              onChange={(e) => this.updateTech1(e.target.value)}
+            />
+            <input
+              type="text"
+              id="tech2"
+              name="tech2"
+              placeholder="Javascript"
+              className="rounded-input"
+              value={this.state.tech2}
+              onChange={(e) => this.updateTech2(e.target.value)}
+            />
+            <input
+              type="text"
+              id="tech3"
+              name="tech3"
+              placeholder="Node.js"
+              className="rounded-input"
+              value={this.state.tech3}
+              onChange={(e) => this.updateTech3(e.target.value)}
+            />
+            {this.validateTech && <ValidationError message={techError} />}
             <div className="form-btn">
               <button
                 type="submit"
                 className="btn"
                 disabled={this.validateTech()}
               >
-                Update
+                Submit
               </button>
             </div>
           </form>
