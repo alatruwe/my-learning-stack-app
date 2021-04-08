b46962cac95596fd2400637f7277392a03091976
adelinel <adeline.latruwe@gmail.com>
2021-04-08 14:44:36 -0700

    updated card styling

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,17 +1,16 @@
 import { Component } from "react";
-import "./LandingPage.css";
 
 class LandingPage extends Component {
   render() {
     return (
       <div className="wrapper">
-        <section className="card landing-page-details">
-          <h2 className="landing-page-title">Elevate Your Learning</h2>
-          <p className="landing-page-text">Keep track of your progress</p>
+        <section className="card">
+          <h2 className="card-title">Elevate Your Learning</h2>
+          <p className="card-text">Keep track of your progress</p>
 
-          <p className="landing-page-text">Identify your struggles</p>
+          <p className="card-text">Identify your struggles</p>
 
-          <p className="landing-page-text">Celebrate your successes</p>
+          <p className="card-text">Celebrate your successes</p>
         </section>
       </div>
     );
